25532daabfe2b5c948202d2d0e61f1f4166f79c6
yuehe <yuehe@yuehes-mac-mini.local>
2018-10-18 11:41:54 +0800

    Initial Commit

---
+++ opengl-triangle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  opengl-triangle
+//
+//  Created by yuehe on 2018/10/18.
+//  Copyright © 2018年 yuehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ opengl-triangle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  opengl-triangle
+//
+//  Created by yuehe on 2018/10/18.
+//  Copyright © 2018年 yuehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
